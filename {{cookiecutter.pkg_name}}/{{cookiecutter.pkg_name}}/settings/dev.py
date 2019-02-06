f02e0b3324f1a65a14adc3571d4c592fd2279cfe
Carl Topham <carl@carl-topham.com>
2019-02-06 10:30:26 +0000

    Removes hardcoded secretkey

--- {{cookiecutter.pkg_name}}/{{cookiecutter.pkg_name}}/settings/dev.py
+++ {{cookiecutter.pkg_name}}/{{cookiecutter.pkg_name}}/settings/dev.py
@@ -1,10 +1,12 @@
+import os
+
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k1@+=wkvv^*reo9y5gt7*i3ytcx37zn933x9$@lfc(n@qk*23z'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ['*'] 
